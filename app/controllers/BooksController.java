0ad55683c1391971776fcfbda74161dd07659390
robuntu <robuntu@robuntu.ca>
2018-04-19 14:15:13 -0400

    finish video 19

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -37,25 +37,52 @@ public class BooksController extends Controller {
     // to save book
 
     public Result save() {
-
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
+        Book book = bookForm.get();
+        Book.add(book);
+        return redirect(routes.BooksController.index());
     }
 
     public Result edit(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if (book == null) {
+            return notFound( "Book Not Found");
+        }
+        Form<Book> bookForm= formFactory.form(Book.class).fill(book);
+        return ok(edit.render(bookForm));
     }
 
     public Result update() {
-        return TODO;
+        Book book = formFactory.form(Book.class).bindFromRequest().get();
+        Book oldBook = Book.findById(book.id);
+        if(oldBook == null) {
+            return notFound("Book Not Found");
+        }
+
+        oldBook.title = book.title;
+        oldBook.author = book.author;
+        oldBook.price = book.price;
+
+        return redirect(routes.BooksController.index());
     }
 
     public Result destroy(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if (book == null) {
+            return notFound("Book not found");
+
+        }
+        Book.remove(book);
+        return redirect(routes.BooksController.index());
     }
 
     //for book details
 
     public Result show(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if (book == null) {
+            return notFound("Book not found");
+        }
+        return ok(show.render(book));
     }
 }
--- app/models/Book.java
+++ app/models/Book.java
@@ -53,4 +53,35 @@ public class Book {
         return books.remove(book);
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
